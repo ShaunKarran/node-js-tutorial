2c23bb800b50d5cc7b4c51b861123136c2dc5ea3
Shaun Karran <shaun.karran@gmail.com>
2016-07-28 13:19:11 +1000

    Add files/changes for heroku.

--- server.js
+++ server.js
@@ -5,20 +5,22 @@ var pug = require('pug');
 var app = express();
 var template = pug.compileFile(__dirname + '/source/templates/homepage.pug');
 
+app.set('port', (process.env.PORT || 5000)); // Save the port value in app so it can be retrieved later.
+
 app.use(logger('dev'));
 app.use(express.static(__dirname + '/static'));
 
-app.get('/', function(req, res, next) {
+app.get('/', function(request, response, next) {
     try {
         var html = template({
             title: 'Home'
         });
-        res.send(html);
+        response.send(html);
     } catch (e) {
         next(e);
     }
 });
 
-app.listen(process.env.PORT || 3000, function() {
-    console.log('Listening on http://localhost:' + (process.env.PORT || 3000));
+app.listen(app.get('port'), function() {
+    console.log('Node app is listening on port ' + app.get('port'));
 });
